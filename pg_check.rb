ffef6835f52913bccb35afbe33027939ad94cc1b
danhnc <congdanh10123@gmail.com>
2018-06-07 11:54:53 +0700

    sample ko co id

--- pg_check.rb
+++ pg_check.rb
@@ -18,14 +18,14 @@ begin
 	row = File.readlines(path)
     con.exec "DROP TABLE IF EXISTS #{filename}"
     att_name = row[0].split(',')
+    puts row[0]
+    puts "=============="
     puts att_name.size
     # puts att_name[0]
     # con.exec "CREATE TABLE #{filename} (#{att_name[0]}  INTEGER PRIMARY KEY)"
     con.exec "CREATE TABLE #{filename} (id INTEGER PRIMARY KEY)"
     att_index = 0
-    puts "================="
     while att_index < att_name.size do
-        puts att_name[att_index]
     	con.exec "ALTER TABLE #{filename} ADD #{att_name[att_index]} TEXT"
     	att_index += 1
     end
@@ -37,7 +37,8 @@ begin
     	col_index = 0
     	while col_index < col.size do
     		# con.exec "UPDATE #{filename} SET #{att_name[col_index]}='#{col[col_index]}' WHERE #{att_name[0]}=#{row_index}"
-            con.exec "UPDATE #{filename} SET #{att_name[col_index]}='#{col[col_index]}' WHERE #{id}=#{row_index+1}"
+            con.exec "UPDATE #{filename} SET #{att_name[col_index]}='#{col[col_index]}' WHERE id=#{row_index}"
+            puts "UPDATE #{filename} SET #{att_name[col_index]}='#{col[col_index]}' WHERE id=#{row_index}"
     		col_index += 1
     	end
     	row_index += 1
